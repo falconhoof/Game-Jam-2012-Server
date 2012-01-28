e8b22dfd60f366636e14f49dbd9e0d162267a052
Lee Reilly <lee@leereilly.net>
2012-01-28 02:19:24 -0800

    Couldn't sleep; made HTML views less ugly

--- lib/web.rb
+++ lib/web.rb
@@ -2,16 +2,19 @@ module Falconhoof
   class Web < Sinatra::Base
 
     get '/' do
+      @title = "Coming soon!"
       erb :index
     end
 
     get '/high-scores/?' do
       @scores = Score.order(:score.desc).limit(20)
+      @title = "High Scores"
       erb :high_scores
     end
 
     get '/statistics/?' do
       @stats = Statistic.all
+      @title = "Statistics"
       erb :statistics
     end
 
